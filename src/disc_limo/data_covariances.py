ae35ab7d52709edddd50a8e0e3618b937ce94825
Tom Hilder <tom.hilder.dlhp@gmail.com>
2025-02-21 15:05:21 +1100

    data covariances operator

---
+++ src/disc_limo/data_covariances.py
@@ -0,0 +1,17 @@
+# data_covariances.py
+# Thomas Hilder
+
+import numpy as np
+import pylops as pl
+from numpy.typing import NDArray
+
+from .convolution_matrix import H_operator
+
+
+def C_operator(
+    rms: float, n_x: int, n_y: int, kernel_array: NDArray
+) -> pl.LinearOperator:
+    # Beam convolution sets the correlation
+    H = H_operator(n_x, n_y, kernel_array)
+    # Get covariances
+    return rms**2 * H * (1 / kernel_array.max())
